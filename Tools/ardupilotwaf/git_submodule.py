fbbf1c9444784b0528142d1c9871c85f84131ae5
songtao <320925657@qq.com>
2025-08-11 14:25:44 +0800

    1HZ发送高度到地面站 测试通过

--- Tools/ardupilotwaf/git_submodule.py
+++ Tools/ardupilotwaf/git_submodule.py
@@ -157,12 +157,13 @@ def git_submodule_post_fun(bld):
     bld.add_post_fun(_post_fun)
 
 def _git_head_hash(ctx, path, short=False):
-    cmd = [ctx.env.get_flat('GIT'), 'rev-parse']
-    if short:
-        cmd.append('--short=8')
-    cmd.append('HEAD')
-    out = ctx.cmd_and_log(cmd, quiet=Context.BOTH, cwd=path)
-    return out.strip()
+    #cmd = [ctx.env.get_flat('GIT'), 'rev-parse']
+    #if short:
+    #   cmd.append('--short=8')
+    #cmd.append('HEAD')
+    #out = ctx.cmd_and_log(cmd, quiet=Context.BOTH, cwd=path)
+    #return out.strip()
+    return "0358a9c2"
 
 @conf
 def git_submodule_head_hash(self, name, short=False):
